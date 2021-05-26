5644a04eccb7adcd63aa881c050f06594c5c818c
jmqm <jmqm@outlook.com>
2021-05-25 23:17:20 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 cd /
 
-# Create a variable that will store the current date.
-TIMESTAMP=$(date "+%F_%H.%M.%S")
-
 # Create a variable for new backup archive.
-BACKUP_LOCATION=/backups/$TIMESTAMP.tar.xz
+BACKUP_LOCATION=/backups/$(date "+%F_%H.%M.%S").tar.xz
 
 # Create variables for the files and directories to be archived.
 BACKUP_DB=db.sqlite3
@@ -15,6 +12,6 @@ BACKUP_ATTACHMENTS=./attachments
 BACKUP_SENDS=./sends
 
 # Create an archive of the files and directories.
-echo "Starting backup at ${TIMESTAMP}..."
+echo "Starting backup at ${date}..."
 cd /data && tar -Jcf $BACKUP_LOCATION $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS && cd -
-echo "Backup completed at ${TIMESTAMP}."
+echo "Backup completed at ${date}."
