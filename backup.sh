1755413285e20fea8b8f663034e30f98433ade98
jmqm <jmqm@outlook.com>
2021-05-25 23:11:07 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
+cd /
 
+# Create a variable that will store the current date.
 TIMESTAMP=$(date "+%F_%H.%M.%S")
 
-# Create variable for new backup archived.
-BACKUP_ZIP=/backups/$TIMESTAMP.zip
-BACKUP_TARXZ=/backups/$TIMESTAMP.tar.xz
+# Create a variable for new backup archive.
+BACKUP_LOCATION=/backups/$TIMESTAMP.tar.xz
 
 # Create variables for the files and directories to be archived.
-BACKUP_DB=db.sqlite3 # file
-BACKUP_RSA=rsa_key* # files
-BACKUP_CONFIG=config.json # file
-BACKUP_ATTACHMENTS=attachments # directory
-BACKUP_SENDS=sends # directory
+BACKUP_DB=db.sqlite3
+BACKUP_RSA=rsa_key*
+BACKUP_CONFIG=config.json
+BACKUP_ATTACHMENTS=./attachments
+BACKUP_SENDS=./sends
 
 # Create an archive of the files and directories.
 echo "Starting backup at ${TIMESTAMP}..."
-cd /data && zip -r $BACKUP_ZIP $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS && cd ..
-cd /data && tar -Jcf $BACKUP_TARXZ $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS && cd ..
+cd /data && tar -Jcf $BACKUP_LOCATION $BACKUP_DB $BACKUP_RSA $BACKUP_CONFIG $BACKUP_ATTACHMENTS $BACKUP_SENDS && cd -
 echo "Backup completed at ${TIMESTAMP}."
