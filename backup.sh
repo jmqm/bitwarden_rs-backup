cc536bf9b0256ee76824c25e558f96a023aca326
jmqm <jmqm@outlook.com>
2021-05-25 22:34:29 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 # Create variable for new backup zip.
 #BACKUP_ZIP=/backups/$(date "+%F_%H.%M.%S").zip
-BACKUP_ZIP=/backups/$(date "+%F_%H.%M.%S").tar.gz
+BACKUP_ZIP=/backups/$(date "+%F_%H.%M.%S").tar.xz
 
 # Create variables for the files and directories to be zipped.
 BACKUP_DB=db.sqlite3 # file
