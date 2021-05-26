94065b1e1b8a10cdedfe835961ff8fd8c868dfce
jmqm <jmqm@outlook.com>
2021-05-26 06:10:25 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 BACKUP_CMD="/sbin/su-exec ${UID}:${GID} /app/backup.sh"
+LOGS_FILE="/app/log/backup.log"
 
 # Run backup script once ($1 = First argument passed).
 if [ "$1" = "manual" ]; then
@@ -9,22 +10,22 @@ if [ "$1" = "manual" ]; then
   exit 0
 fi
 
-# Initialize cron
+# Create cron jobs.
 if [ "$(id -u)" -eq 0 ] && [ "$(grep -c "$BACKUP_CMD" "$CRONFILE")" -eq 0 ]; then
-  echo "$CRON_TIME $BACKUP_CMD" | crontab -
+  echo "$CRON_TIME $BACKUP_CMDD >> $LOGS_FILE 2>&1" | crontab -
+  # Delete after x days job here.
 fi
 
-# Start crond if it's not running
+# Start crond if it's not running.
 pgrep crond > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   /usr/sbin/crond -L /app/log/cron.log
 fi
 
-# Restart script as user "app:app"
+# Restart script as user "app:app".
 if [ "$(id -u)" -eq 0 ]; then
   exec su-exec app:app "$0" "$@"
 fi
 
-echo "[INFO] Running automatically (${CRON_TIME}), started at $(date +"%F %r")."
-tail -f /dev/null # Keeps container running.
-#tail -F "$LOGFILE" /app/log/cron.log
+echo "[INFO] Running automatically (${CRON_TIME}), started at $(date +"%F %r")."# > "$LOGS_FILE"
+tail -F "$LOGS_FILE" /app/log/cron.log # Keeps terminal open and writes logs.
