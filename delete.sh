6546e73cbebb5c9c2dc1eeeb188d03fe77df5318
jmqm <jmqm@outlook.com>
2021-07-17 14:35:18 -0500

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -9,15 +9,14 @@ cd /backups
 # Find all tar.xz archives older than x days and store them in a variable.
 TO_DELETE=$(find . -iname "*.tar.xz" -type f -mtime +$DELETE_AFTER)
 
-if [ "$TO_DELETE" -gt 0 ]; then
+# Check if TO_DELETE is empty.
+if [ ! -z "$TO_DELETE" ]; then
     # Delete tar.xz archives older than x days.
-    #find . -iname "*.tar.xz" -type f -mtime +$DELETE_AFTER -exec rm -f {} \;
-    echo "$TO_DELETE"
-    rm -f "$TO_DELETE"
+    find . -iname "*.tar.xz" -type f -mtime +$DELETE_AFTER -exec rm -f {} \;
 
     # Echo that archives were deleted.
     echo "[$(date +"%F %r")] Deleted archives older than $DELETE_AFTER days."
 else
     # Echo that there are no archives to delete.
-    echo "[$(date +"%F %r")] No archives older than $DELET_AFTER days to delete."
+    echo "[$(date +"%F %r")] No archives older than $DELETE_AFTER days to delete."
 fi
